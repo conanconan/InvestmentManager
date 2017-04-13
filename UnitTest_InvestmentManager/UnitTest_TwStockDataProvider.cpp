000a5ecbf597dfdaec454236c3051c8afc907e18
conan <conan.cnke@gmail.com>
2017-04-13 14:51:53 +0800

    add unit test project

---
+++ UnitTest_InvestmentManager/UnitTest_TwStockDataProvider.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest_InvestmentManager
+{
+    TEST_CLASS(UnitTest_TwStockDataProvider)
+    {
+    public:
+        TEST_METHOD(GetAgentIdList)
+        {
+            // TODO: Your test code here
+        }
+    };
+}
