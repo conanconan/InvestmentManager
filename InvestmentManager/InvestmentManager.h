d7cec01aca3b9bd27f35d84081cca0c4c0ddf662
conan <conan.cnke@gmail.com>
2017-04-13 14:23:46 +0800

    fix bug. define INVESTMGR_DLLAPI for import dll

--- InvestmentManager/InvestmentManager.h
+++ InvestmentManager/InvestmentManager.h
@@ -4,7 +4,7 @@
 #ifdef _INVESTMGRDLL
 #define INVESTMGR_DLLAPI extern "C" __declspec(dllexport)
 #else
-#define INVESTMGR_API extern "C" __declspec(dllimport)
+#define INVESTMGR_DLLAPI extern "C" __declspec(dllimport)
 #endif
 
 INVESTMGR_DLLAPI bool CreateInvestMgr(IInvestMgr** ppInvestMgr);
